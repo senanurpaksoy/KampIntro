adfc04c29bf9ea3cae046f9f1260927d87281107
senanurpaksoy <senapksy@gmail.com>
2021-01-23 23:51:53 +0300

    23 Ocak Dersi

---
+++ OOP2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace OOP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GercekMusteri musteri1 = new GercekMusteri();
+            musteri1.Id = 1;
+            musteri1.MusteriNo = "3244";
+            musteri1.Adi = "Sena";
+            musteri1.Soyadi = "Paksoy";
+            musteri1.TcNo = "2425254532456";
+
+
+            //Kodlama.io
+
+            TuzelMusteri musteri2 = new TuzelMusteri();
+            musteri2.Id = 2;
+            musteri2.MusteriNo = "32435";
+            musteri2.SirketAdi = "Kodlama.io";
+            musteri2.VergiNo = "2423535345464367";
+
+            Musteri musteri3 = new GercekMusteri();
+            Musteri musteri4 = new TuzelMusteri();
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri1);
+            musteriManager.Ekle(musteri2);
+
+        }
+    }
+}
---
+++ OOP3/BasvuruManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP3
+{
+    class BasvuruManager
+    {
+        //Method injection
+        public void BasvuruYap(IKrediManager krediManager, ILoggerService loggerService)
+        {
+            //Başvuran bilgilerini değerlendirme
+            //bu sadece tek bir taneyi hesaplıyor
+            krediManager.Hesapla();
+            loggerService.Log();
+        }
+
+        public void KrediOnBilgilendirmesiYap(List<IKrediManager> krediler)
+        {
+            //listeye eklenenleri hesaplıyor
+            foreach (var kredi in krediler)
+            {
+                kredi.Hesapla();
+            }
+        }
+    }
+}
